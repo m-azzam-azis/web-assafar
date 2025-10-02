f148abe46520e179b9041127a464d73a1a41f642
m.azzam <m.azzam.azis@gmail.com>
2025-10-02 19:18:09 +0700

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { El_Messiri } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 import Footer from "@/components/layout/Footer";
 
@@ -100,6 +101,7 @@ export default function RootLayout({
       </head>
       <body className={` ${elMessiri.variable} antialiased `}>
         <main>{children}</main>
+        <Analytics />
         <Footer />
       </body>
     </html>
